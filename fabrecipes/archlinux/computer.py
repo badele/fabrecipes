e8f6bb835412a1595593c421939307f7e4593609
badele <bruno.adele@jesuislibre.org>
2013-06-05 14:23:08 +0200

    Change xorg variable into pkgs

--- fabrecipes/archlinux/computer.py
+++ fabrecipes/archlinux/computer.py
@@ -15,7 +15,7 @@ def computer_sample():
     env.keymap = 'fr-pc'
     env.timezone_continent = 'Europe'
     env.timezone_city = 'City'
-    env.xorg = ['virtualbox-guest-utils']
+    env.pkgs = ['virtualbox-guest-utils']
     env.arch = 'x86_64'
     env.disk = '/dev/sda'
     env.part = {
@@ -31,6 +31,52 @@ def computer_sample():
             'ptype': 'Linux',
             'ftype': 'ext4',
             'size': '1.5g'
+        },
+        '/boot': {
+            'device': '/dev/sda1',
+            'ptype': 'Linux',
+            'ftype': 'ext2'
+        },
+        'swap': {
+            'device': '/dev/sda2',
+            'ptype': 'Linux swap / Solaris',
+            'ftype': 'swap'
+        },
+    }
+
+
+@task
+def jsl_acerone():
+    """
+    Acer Aspire One netbook
+    installed on xx minutes
+    fab -f autoinstall/archlinux.py -H root@hostname computer_sample install
+    fab -f autoinstall/archlinux.py -H root@hostname computer_sample configure env_i3 sync_dotfiles
+    """
+    env.hostname = 'acerone'
+    env.useraccount = 'badele'
+    env.dotfiles = 'https://github.com/badele/dotfiles.git'
+    env.locale = 'fr_FR.UTF-8'
+    env.charset = 'UTF-8'
+    env.keymap = 'fr-pc'
+    env.timezone_continent = 'Europe'
+    env.timezone_city = 'City'
+    env.pkgs = ['xf86-video-intel', 'xf86-input-synaptics']
+    env.arch = 'x86_64'
+    env.disk = '/dev/sda'
+    env.part = {
+        'lvm': {'device': '/dev/sda3', 'ptype': 'Linux'},
+        '/': {
+            'device': '/dev/vg/root',
+            'ptype': 'Linux',
+            'ftype': 'ext4',
+            'size': '30g'
+        },
+        '/home': {
+            'device': '/dev/vg/home',
+            'ptype': 'Linux',
+            'ftype': 'ext4',
+            'size': '100g'
         },
         '/boot': {
             'device': '/dev/sda1',
