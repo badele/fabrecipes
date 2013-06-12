6c85b4aa156b1130ff3e235637b7c868dccaa20c
badele <bruno.adele@jesuislibre.org>
2013-06-12 22:15:55 +0200

    Add wine package installation

---
+++ fabrecipes/archlinux/wine/fabfile.py
@@ -0,0 +1,52 @@
+import os
+
+# Fabric
+from fabric.api import settings, env, task, sudo, run, local
+from fabric.utils import abort
+from fabric.colors import red
+from fabric.operations import prompt, reboot, put
+
+# Fabtools
+from fabtools.require import file as require_file
+from fabtools.utils import run_as_root
+from fabric.contrib.files import append, comment, uncomment, sed
+from fabtools.files import watch, is_dir, is_link
+from fabtools import require
+from fabtools import python
+from fabtools import system
+from fabtools import arch
+from fabtools import disk
+
+#from computer import *
+
+"""
+   This script install wine with :
+   - configure wine in 32 bits
+   - install flash11
+"""
+
+
+@task
+def install():
+    """
+    Install wine with customization
+    """
+    if 'pkgs' not in env:
+        env.pkgs = []
+
+    pkgs = [
+        'wine',
+        'wine_gecko',
+        'wine-mono',
+        'winetricks-svn',
+        'lib32-libxml2',
+        'lib32-mpg123',
+        'lib32-giflib',
+        'lib32-libpng',
+        'lib32-gnutls',
+    ]
+    require.arch.packages(pkgs)
+
+    run('winecfg')
+    cmd = 'winetricks firefox flash11'
+    run('export DISPLAY=:0 && %s' % cmd)
