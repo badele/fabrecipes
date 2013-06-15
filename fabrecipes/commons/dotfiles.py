fa6e6f7c2e32914d699f6ee8ff5a6c35e754673b
badele <bruno.adele@jesuislibre.org>
2013-06-15 14:46:51 +0200

    - Check if localy dotfiles exist - In root, use environement fabric user dotfiles

--- fabrecipes/commons/dotfiles.py
+++ fabrecipes/commons/dotfiles.py
@@ -1,21 +1,22 @@
 # Fabric
 from fabric.api import env, task, sudo, run
+from fabric.utils import abort
 
 # Fabtools
-from fabtools.utils import run_as_root
-from fabtools.files import is_dir, is_link
-
+#from fabtools.utils import run_as_root
+from fabtools.files import is_dir
+from fabric.colors import red
 
 @task
-def fetch(repository):
+def fetch(git):
     """
     Clone dotfiles project to ~/dotfiles
     """
     cloned = False
     # Check project is already cloned
 
-    if not is_dir('$HOME/dotfiles'):
-        cmd = 'cd ; git clone %(repository)s' % locals()
+    if not is_dir('/home/%(user)s/dotfiles' % env):
+        cmd = 'cd ; git clone %(git)s' % locals()
         run(cmd)
         cloned = True
 
@@ -32,8 +33,16 @@ def sync(src, dst, use_sudo='false'):
     Copy file from dotfiles dot dst
     """
     use_sudo = use_sudo.lower() == 'true'
+    dotfiles = '/home/%(user)s/dotfiles' % env
+
+    # Update dotfiles
+    if not is_dir(dotfiles):
+        abort(red("Please execute dotfiles.fetch"))
+    run('cd %(dotfiles)s ; git pull' % locals())
 
     # Synchronize system
-    dotfiles = '$HOME/dotfiles' 
-    cmd = 'rsync -avr --exclude ".git/" %(dotfiles)s/%(src)s %(dst)s' % locals()
-    print(cmd)
+    cmd = 'rsync -avr --exclude ".git/" "%(dotfiles)s/%(src)s" "%(dst)s"' % locals()
+    if use_sudo:
+        sudo(cmd)
+    else:
+        run(cmd)
