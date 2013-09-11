feb7e98a61647cd9d6fe163c3511486dfe73f0b9
badele <bruno.adele@jesuislibre.org>
2013-09-11 18:43:11 +0000

    Add a pelican package

---
+++ fabrecipes/archlinux/pelican/__init__.py
@@ -0,0 +1,81 @@
+import os
+
+# Fabric
+from fabric.api import env, task, run
+from fabric.operations import put
+from fabric.context_managers import cd
+
+# Fabtools
+from fabtools.files import is_dir
+from fabtools import require
+from fabtools import python
+
+
+"""
+   Install pelilcan
+"""
+
+
+@task
+def install():
+    """
+    Install a pelilcan in python2 virtualenv
+    """
+
+    if not env.host_string:
+        env.host_string = 'localhost'
+
+    pkgs = [
+        'python2',
+        'git',
+    ]
+    require.arch.packages(pkgs)
+
+    project_root = '$HOME/projects'
+    project_name = 'pelican'
+    use_python = 'python2.7'
+    virtualenv = '.virtualenvs/pelican'
+    gitproject = 'https://github.com/getpelican/pelican.git'
+
+    require.python.pip(python_cmd=use_python)
+    require.python.package(
+        'virtualenv',
+        python_cmd=use_python,
+        use_sudo=True,
+    )
+    require.python.package(
+        'virtualenvwrapper',
+        python_cmd=use_python,
+        use_sudo=True,
+    )
+    require.python.virtualenv(
+        virtualenv,
+        python_cmd=use_python,
+        venv_python='python2.7',
+    )
+
+    # Get a pelilcan github repository github.com/getpelican/pelican.git
+    cloned = False
+    project = '%s/%s' % (project_root, project_name)
+    if not is_dir(project):
+        require.files.directory(project_root)
+        cmd = 'cd %s ; git clone %s' % (project_root, gitproject)
+        run(cmd)
+        cloned = True
+
+    if not cloned and not is_dir(project):
+        cmd = 'cd (%project)s ; git pull' % locals()
+        run(cmd)
+
+
+    # Install pelilcan
+    with python.virtualenv(virtualenv):
+        here = os.path.dirname(__file__)
+        requirements = '%(here)s/requirements.txt' % locals()
+        put(requirements, '/tmp/requirements.txt')
+        require.python.requirements(
+            '/tmp/requirements.txt',
+        )
+
+        with cd(project):
+            run('python2 setup.py install')
---
+++ fabrecipes/archlinux/pelican/fabfile.py
@@ -0,0 +1,1 @@
+from fabrecipes.archlinux.pelican import *
