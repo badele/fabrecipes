7464e51aa11c8ec7802f8682b989a3b3fcd7d810
badele <bruno.adele@jesuislibre.org>
2013-10-21 02:12:51 +0200

    Add test capabalities on the new exotic system, ex: OpenWrt

--- fabrecipes/fabfile.py
+++ fabrecipes/fabfile.py
@@ -1,3 +1,26 @@
+# Fabric
+from fabric.api import env, task, run, shell_env
+from fabtools import system
+
+# Fabrecipes
 import archlinux
 from fabrecipes import computer
 from fabrecipes.commons import dotfiles
+
+
+@task
+def capabilities():
+    """Test capabilities functions in new distrition
+    For openwrt:
+      mv /etc/banner /etc/banner.disable
+      fab -s "/bin/sh -l -c" capabilities
+    """
+
+    print ("SYSTEM")
+    print ("======")
+    print("Distribution: %s" % system.distrib_id())
+    print("Release: %s" % system.distrib_release())
+    print("Codename: %s" % system.distrib_codename())
+    print("Desc: %s" % system.distrib_desc())
+    print("Arch: %s" % system.get_arch())
+    print("Hostname: %s" % system.get_hostname())
