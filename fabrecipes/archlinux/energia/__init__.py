b9c6d517973c195fa81df8494631ad155a948b3b
badele <bruno.adele@jesuislibre.org>
2015-03-15 10:06:00 +0100

    Change user group

--- fabrecipes/archlinux/energia/__init__.py
+++ fabrecipes/archlinux/energia/__init__.py
@@ -10,14 +10,14 @@ from fabtools.files import is_dir
 from fabtools import require
 
 """
-   Install energia developpement kit
+   Install energia toolchain developpement kit
 """
 
 
 @task
 def install():
     """
-    Install a energia developpement kit
+    Install a energia toolchain developpement kit
     """
 
     if not env.host_string:
@@ -30,6 +30,6 @@ def install():
     require.arch.packages(pkgs)
 
     # Install udev
-    ruled = 'ATTRS{idVendor}=="0451", ATTRS{idProduct}=="c32a", MODE="0660", GROUP="badele", \
+    ruled = 'ATTRS{idVendor}=="0451", ATTRS{idProduct}=="c32a", MODE="0660", GROUP="users", \
 RUN+="/sbin/modprobe ftdi-sio" RUN+="/bin/sh -c \'/bin/echo 0451 c32a > /sys/bus/usb-serial/drivers/ftdi_sio/new_id\''
     require.file('/etc/udev/rules.d/energia.rules', contents=ruled, use_sudo=True)
