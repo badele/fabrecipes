7234fd8e78f3c75eb12c47ae77a71311bdfe752a
badele <bruno.adele@jesuislibre.org>
2015-06-17 23:47:16 +0200

    Add debian requirements

---
+++ fabrecipes/debian/__init__.py
@@ -0,0 +1,2 @@
+from fabrecipes.debian import hosting
+from fabrecipes.debian import docker
---
+++ fabrecipes/debian/fabfile.py
@@ -0,0 +1,3 @@
+from fabrecipes.debian import hosting
+from fabrecipes.debian import proxmox
+from fabrecipes.debian import docker
